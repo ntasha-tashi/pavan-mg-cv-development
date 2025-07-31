8c756c132d08f0284b5a09df5e5029c723e33cca
ntasha-tashi <zamida.natasa@gmail.com>
2025-07-31 11:30:00 +0200

    add projects

--- src/js/Pages/Home.js
+++ src/js/Pages/Home.js
@@ -3,9 +3,6 @@ import Header from "../Components/Header/Header";
 import Banner from "../Components/Banner/Banner";
 import Section from "../Components/Section/Section";
 import Social from "../Components/Header/Social/Social";
-import gh from "../../assets/github.svg";
-import tw from "../../assets/twitter.svg";
-import ln from "../../assets/linkedin.svg";
 
 import html from "../../assets/html.svg";
 import css from "../../assets/css.svg";
@@ -15,13 +12,14 @@ import sass from "../../assets/sass.svg";
 import git from "../../assets/git.svg";
 import code from "../../assets/code.svg";
 import gith from "../../assets/gith.svg";
+import Projects from "../Components/Projects/Projects";
 
 const Home = () => {
     return (
         <>
             <Header type="header" />
             <Banner/>
-            <Section type="tech" title="My Tech Stack" subtitle="Technologies I’ve been working with recently">
+            <Section type="tech" title="My Tech Stack" subtitle="Technologies I've been working with recently">
                 <Social type={"tech"} items={[
                     { url: "#", src: html, alt: "HTML" },
                     { url: "#", src: css, alt: "CSS" },
@@ -34,7 +32,9 @@ const Home = () => {
 
                 ]}/>
             </Section>
-            <Section type="projects" title="Projects" subtitle="Things I’ve built so far"/>
+            <Section type="projects" title="Projects" subtitle="Things I've built so far">
+                <Projects/>
+            </Section>
             <Header type="footer"/>
         </>
     )
